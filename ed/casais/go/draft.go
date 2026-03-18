a10e51575401b76a129806970d901f406c7fb785
rayca-alves <annarayca1@gmail.com>
2026-03-18 14:18:21 +0000

    respostaCasais

--- ed/casais/go/draft.go
+++ ed/casais/go/draft.go
@@ -3,37 +3,55 @@ package main
 import "fmt"
 
 func main() {
-	var N, i int
+	qtd := 0
+	fmt.Scan(&qtd)
+	animais := make([]int, qtd)
+
+	solteiros := make(map[int]int)
+	for i := range animais{
+		fmt.Scan(&animais[i])
+	}
+	n_pares := 0
+
+	for _, animal := range animais{
+		if solteiros[-animal] > 0{
+			solteiros[-animal]--
+			n_pares++
+		}else{
+			solteiros[animal]++
+		}
+	}
+
+	fmt.Println(n_pares)
+	/*var N, i int
 	fmt.Scan(&N)
 
 	var animais [50]int
-	var f [100]int
 	var i_f, im int
-	var m [100]int
-	var pares int
-	casado := false
+	var casais int
+	var casado [100]bool
 
 	for i = 0; i < N; i++ {
 		fmt.Scan(&animais[i])
 		if animais[i] < 0 {
 			i_f++
-			f[i] = animais[i]
 		} else {
 			im++
-			m[i] = animais[i]
 		}
 		for j := 1; j < i_f; j++ {
 			for x := 1; x < im; i++ {
 
-				if casado == false && m[x] == (f[j]*-1) {
-					pares++
-					casado = true
+				if casado[j] == false && animais[x] == (animais[j]*-1) {
+					casais++
+					casado[j] = true
+				}else{
+					casado[j] = false
 				}
 			}
 		}
 
 	}
 
-    fmt.Println(pares)
-
+    fmt.Println(casais)
+*/
 }
