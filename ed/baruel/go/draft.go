a225373aea397112395776d849425fe3793a36f7
rayca-alves <annarayca1@gmail.com>
2026-03-22 17:58:17 +0000

    salvar

--- ed/baruel/go/draft.go
+++ ed/baruel/go/draft.go
@@ -5,13 +5,15 @@ import "fmt"
 func main() {
     var qtd_album, qtd_possui int
     fmt.Scan(&qtd_album, &qtd_possui)
+
     figurinhas := make([]int, qtd_possui)
     for i := range figurinhas{
         fmt.Scan(&figurinhas[i])
     }
-    fmt.Println(qtd_album, qtd_possui, figurinhas)
+
     unicos := make(map[int]bool)
     repetidos := make([]int, 0, qtd_possui)
+
     for _, figura := range figurinhas{
         if unicos[figura]{
             repetidos = append(repetidos, figura)
@@ -19,10 +21,12 @@ func main() {
             unicos[figura] = true
         }
     }
-
     saida := fmt.Sprintf("%v", repetidos)
-    fmt.Println(saida[1 : len(saida)-1])
-
+    if saida == "[]"{
+        fmt.Println("N")
+    }else{
+         fmt.Println(saida[1 : len(saida)-1])
+    }
     faltantes := make([]int, 0, qtd_album)
     for i := 1; i <= qtd_album; i++{
         if !unicos[i]{
@@ -31,7 +35,10 @@ func main() {
     }
 
     saida_f := fmt.Sprintf("%v", faltantes)
-    fmt.Println(saida[1 : len(saida_f)-1])
-    
+    if saida_f == "[]"{
+        fmt.Println("N")
+    }else{
+        fmt.Println(saida_f[1 : len(saida_f)-1])
+    }
 
 }
