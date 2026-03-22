d96a758cc4a073c8fafc32debad4038fef416e32
rayca-alves <annarayca1@gmail.com>
2026-03-22 19:11:11 +0000

    salvar

--- ed/gomos/go/draft.go
+++ ed/gomos/go/draft.go
@@ -1,5 +1,36 @@
 package main
 import "fmt"
+
+type Gomo struct{
+    x, y int
+}
 func main() {
-    fmt.Println("qxcode")
+    var Q int
+    dire := "" 
+    fmt.Scan(&Q, &dire)
+
+    cobra := make([]Gomo, Q)
+
+    for i := range cobra {
+        fmt.Scan(&cobra[i].x, &cobra[i].y)
+    }
+
+    for i := Q - 1; i > 0; i--{
+        cobra[i] = cobra[i-1]
+    }
+
+    switch dire{
+    case "L":
+        cobra[0].x--
+    case "R":
+        cobra[0].x++
+    case "U":
+        cobra[0].y--
+    case "D":
+        cobra[0].y++
+    }
+
+    for _, gomo := range cobra{
+        fmt.Printf("%v %v\n", gomo.x, gomo.y)
+    }
 }
