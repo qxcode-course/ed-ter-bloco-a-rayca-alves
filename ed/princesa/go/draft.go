9afc9bcc88c0d1ab5239da185df6a03959ed6314
rayca-alves <annarayca1@gmail.com>
2026-03-24 14:25:12 +0000

    almoço

--- ed/princesa/go/draft.go
+++ ed/princesa/go/draft.go
@@ -1,5 +1,15 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("qxcode")
+    var n, e int
+    fmt.Scan(&n, &e)
+
+    fila := make([]int, 1, n)
+    mortos := make(map[int]int)
+
+    for matar := range fila{
+        if fila[matar] == e{
+            mortos[matar] = fila[matar+1]
+        }
+    }
 }
