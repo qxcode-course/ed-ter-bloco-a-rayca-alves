bbfc03b25fcc12b7d61dc798e5859dc811483c44
rayca-alves <annarayca1@gmail.com>
2026-03-29 19:55:40 +0000

    Princesa

--- ed/princesa/go/draft.go
+++ ed/princesa/go/draft.go
@@ -1,15 +1,53 @@
 package main
+
 import "fmt"
+
+func print(fila []int, pos int) {
+	fmt.Print("[ ")
+	for i := 0; i < len(fila); i++ {
+		if i == pos {
+			fmt.Printf("%d> ", fila[i])
+		} else {
+			fmt.Printf("%d ", fila[i])
+		}
+	}
+	fmt.Println("]")
+}
+
 func main() {
-    var n, e int
-    fmt.Scan(&n, &e)
+	var n, e int
+	fmt.Scan(&n, &e)
 
-    fila := make([]int, 1, n)
-    mortos := make(map[int]int)
+	fila := make([]int, n)
+	for i := 0; i < n; i++ {
+		fila[i] = i + 1
+	}
 
-    for matar := range fila{
-        if fila[matar] == e{
-            mortos[matar] = fila[matar+1]
-        }
-    }
+	mortos := make(map[int]bool)
+	pos := e - 1
+
+	for len(fila) > 1 {
+		print(fila, pos)
+
+		morto := (pos + 1) % len(fila)
+		mortos[fila[morto]] = true
+
+		nova := []int{}
+		for _, p := range fila {
+			if !mortos[p] {
+				nova = append(nova, p)
+			}
+		}
+
+        if morto > pos {
+			pos = (pos + 1) % len(nova)
+		} else {
+			pos = pos % len(nova)
+		}
+
+		fila = nova
+	}
+
+	
+	print(fila, pos)
 }
