029f90f8482bb5d2835115ab74fea5de224a9145
rayca-alves <annarayca1@gmail.com>
2026-03-29 21:00:22 +0000

    apagando

--- ed/apagando/go/draft.go
+++ ed/apagando/go/draft.go
@@ -1,7 +1,36 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("qxcode")
-    
+    var N, M int
+    fmt.Scan(&N)
+    fila := make([]int, N)
+    for i := 0; i < N; i++{
+        fmt.Scan(&fila[i])
+    } 
+
+    fmt.Scan(&M)
+    apagado := make(map[int]bool)
+    sair := make([]int, M)
+    for i := 0; i < M; i++{
+        fmt.Scan(&sair[i])
+        apagado[sair[i]] = true
+    } 
+
+    novaFila := []int{}
+    for _, f := range fila{
+        if !apagado[f]{
+            novaFila = append(novaFila, f)
+        }
+    }
+    fila = novaFila
+
+    saida := fmt.Sprintf("%v", fila)
+    if saida == "[]"{
+        fmt.Printf("N ")
+        
+    }else{
+        fmt.Printf(saida[1 : len(saida) -1])
+        fmt.Println(" ")
+    }
 }
 
